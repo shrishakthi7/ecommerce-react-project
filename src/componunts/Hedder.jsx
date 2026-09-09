c4e5edff1c9819969300ba75fe7b3ff3991c06fa
Shri Shakthi Velu C <shakthishri88@gmail.com>
2026-09-09 20:34:26 +0530

    Small change

--- src/componunts/Hedder.jsx
+++ src/componunts/Hedder.jsx
@@ -19,7 +19,7 @@ const Hedder = () => {
             <div className="search">
                  <form action="">
                     <input type="text" placeholder='Search items' />
-                    <button>Search</button>
+                    <button>Searc</button>
                  </form>
             </div>
             <div className="btns">
